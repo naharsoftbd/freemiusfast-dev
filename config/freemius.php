aec1be0c7ca9fa28a5ad0b864bf42f3b5a57294b
Tajwar <naharsoftbd@gmail.com>
2026-02-08 10:08:12 +0600

    Env update

--- config/freemius.php
+++ config/freemius.php
@@ -2,10 +2,9 @@
 
 return [
     'product_id' => env('VITE_FREEMIUS_PRODUCT_ID'),
-    'plan_id' => env('FREEMIUS_PLAN_ID'),
-    'public_key' => env('FREEMIUS_PUBLIC_KEY'),
-    'secret_key' => env('FREEMIUS_SECRET_KEY'),
-    'bearer_token' => env('FREEMIUS_API_KEY'),
+    'public_key' => env('pk_093f9a27fc7947b12fad3bac16446'),
+    'secret_key' => env('VITE_FREEMIUS_SECRET_KEY'),
+    'bearer_token' => env('VITE_FREEMIUS_API_KEY'),
     'base_url' => env('VITE_FREEMIUS_BASE_URL'),
     'public_url' => env('VITE_FREEMIUS_PUBLIC_URL'),
     'api_base_url' => env('VITE_FREEMIUS_API_BASE_URL'),
